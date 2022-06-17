6dfa455c657bd85c2aae3c2e935fe97035912d79
Oliver Chang <ochang@xpanxion.com>
2022-06-17 17:54:00 -0400

    Exercise4 comments

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -8,6 +8,7 @@ public class Exercise4 {
         int n = input.nextInt();
         int sum = 0;
         int digit = 0;
+        // Store n into num. This is to keep n intact, so that we can print it in our string later.
         int num = n;
 
         // Check if negative, return same value but positive
@@ -17,9 +18,18 @@ public class Exercise4 {
 
         // Find and print sum of all digits in integer n
         while(num > 0){
+            // If num is 14, 14 % 10 = 4. This would represent the last digit of the number n.
             digit = num % 10;
-            sum = sum + digit;
+            // We take num which is 4, and add it to sum (which is 0 initially). sum will store the total of all digits
+            sum += digit;
+
+            // num / 10 will turn 14 into 1.4 and return the whole integer 1. Division only returns whole integers, not decimals.
+            // We turn num into a decimal (1.4) and chop off the .4 by using the division's ability to round off the integer.
+            // We are left with the remaining digit(s) which are stored into num
             num = num / 10;
+
+            // num will store 1, which is the remaining digit of n (originally 14)
+            System.out.println(num);
         }
         System.out.println(String.format("The sum of all digits of %d is: ", n) + sum);
 
