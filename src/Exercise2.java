0ee0b735cf82519162e1fb0addac0dac8b7698de
Oliver Chang <ochang@xpanxion.com>
2022-06-17 14:40:52 -0400

    Exercise2

---
+++ src/Exercise2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter integer n: ");
+        int n = input.nextInt();
+        int count = 0;
+        int sum = 0;
+
+        while(count <= n) {
+            // Find even numbers. Sum and print even numbers
+            if(count % 2 == 0) {
+                sum += count;
+                }
+            count++;
+            }
+        String msg = String.format("The sum of all even numbers between 1 and %d is: ", n);
+        System.out.println(msg + sum);
+
+        }
+    }
+
