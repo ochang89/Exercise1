71871c35be9367c6e9c918842e97abe59d43c6c4
Oliver Chang <ochang@xpanxion.com>
2022-06-17 15:52:02 -0400

    added comments to exercise1

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -7,8 +7,11 @@ public class Exercise1 {
         int x = input.nextInt();
         int count = 1;
 
+        // Stop loop after count reaches x
         while(count <= x){
+            // For all not even numbers (odd)
             if(count % 2 != 0){
+                // Find the numbers that are multiples of 3, then print them
                 if(count % 3 == 0){
                     System.out.println(count + " is a multiple of 3");
                 }
